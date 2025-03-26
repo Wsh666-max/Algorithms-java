e15f9a8d7099d1b6bc77133ee1459ec222f8a0c3
Defu <2445210373@qq.com>
2025-03-26 14:22:48 +0800

    按值传递、按引用传递、从堆栈的角度解释链节点、以堆栈的视角看链表的反转

---
+++ src/com/atdefu005/ListReverse.java
@@ -0,0 +1,22 @@
+package com.atdefu005;
+
+import java.util.jar.Manifest;
+
+/**
+ * ClassName: ListReverse
+ * Package: com.atdefu005
+ * Description:
+ *
+ * @Author 华因斯坦
+ * @Create 2025/3/26 14:15
+ * @Version 1.0
+ */
+
+//按值传递、按引用传递
+//从堆栈的角度解释链表节点
+//以堆栈的视角看链表的反转
+public class ListReverse {
+    public static void main(String[] args) {
+        
+    }
+}
