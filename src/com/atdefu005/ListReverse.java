c8d3c2d9b2bb2aedf66a21a2b2c47a91828e2dbb
Defu <2445210373@qq.com>
2025-04-01 17:33:06 +0800

    按值传递、按引用传递、从堆栈的角度解释链节点、以堆栈的视角看链表的反转

--- src/com/atdefu005/ListReverse.java
+++ src/com/atdefu005/ListReverse.java
@@ -19,4 +19,49 @@ public class ListReverse {
     public static void main(String[] args) {
         
     }
+
+
+    //单链表（结构）
+    public static class ListNode{
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //单链表反转
+    public static ListNode reverseList(ListNode head){
+        ListNode pre = null;
+        ListNode next = null;
+        while(head != null){
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    //双链表节点
+    public static class DoubleListBode{
+        public int value;
+        public DoubleListBode last;
+        public DoubleListBode next;
+
+        public DoubleListBode(int value) {
+            this.value = value;
+        }
+    }
+
+    //反转双链表
+    //没有找到测试链接
+//    public static DoubleListBode
+
 }
